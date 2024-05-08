6d0c5cb90edbd288729de268d22789150100f113
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-05-08 18:11:14 +0530

    Selectors Started

---
+++ 01_selectors.js
@@ -0,0 +1,11 @@
+const id_selector = () => {
+
+    const head_div = document.getElementById("head-div-id") // Wo Particlular element SELECT HOGA
+
+    head_div.style.border = "3px solid cyan"
+    head_div.style.margin = "20px"
+    head_div.style.padding = "20px"
+
+
+
+}
