d86507a8952fb1387622a25d0f7f40b81b4f9da0
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-05-11 23:54:24 +0530

    Read the Webpage

---
+++ DOM_CRUD/read.js
@@ -0,0 +1,9 @@
+
+
+const div1_text = document.querySelector("#div1")
+
+console.log(div1_text.innerHTML)
+console.log(div1_text.innerText)
+console.log(div1_text.textContent)
+
+// YEHI TOH HAI READ OPERATION!!!
