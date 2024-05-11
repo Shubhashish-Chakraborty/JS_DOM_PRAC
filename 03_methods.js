072896d45ffb35b2e9a05bbf9096ebb3889eceac
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-05-11 18:23:56 +0530

    Style Manipulation

--- 03_methods.js
+++ 03_methods.js
@@ -1,3 +1,26 @@
+const do_Styling = () => {
+
+
+    const languages = document.querySelectorAll(".ListItem")
+    const languages_div = document.querySelectorAll(".list-div")
+
+
+    languages.forEach((e) => {
+
+        e.style.backgroundColor = "black"
+        e.style.color = "cyan"
+        
+        e.style.border = "4px solid red"
+        e.style.borderRadius = "50px"
+
+
+        
+    })
+
+    
+}
+
+
 const about_getAttribute = () => {
 
     const paras = document.querySelectorAll(".para")
